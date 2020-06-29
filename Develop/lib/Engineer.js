f0fceb23219bf9533fb76e92ec9c0a390661cd11
Brad Williams <jb.wms89@gmail.com>
2020-06-29 17:58:41 -0400

    added base class employee and started work with engineer

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,15 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./lib/Employee");
+
+class Engineer extends Employee {
+    constructor(name, role, email, id, officeNumber){
+        super(name, role, email, id);
+        this.officeNumber = officeNumber;
+    }
+
+    getOfficeNumber(){
+        return this.officeNumber;
+    }
+}
+
+module.exports = Engineer;
